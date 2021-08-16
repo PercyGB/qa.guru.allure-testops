3e6c65be0f4d29edae8419c6835922ea171f316a
PercyGB <kuznetsovalv@yandex.ru>
2021-08-16 11:58:18 +0400

    Owner added

---
+++ src/test/java/config/Config.java
@@ -0,0 +1,52 @@
+package config;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.sql.SQLOutput;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class Config {
+
+    @BeforeAll
+    public static void setup() {
+        String login = Credentials.credentials.login();
+        String password = Credentials.credentials.password();
+        String remoteUrl = System.getProperty("selenoidUrl");
+        String selenoidUrl = String.format("https://%s:%s@%s", login, password, remoteUrl);
+
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.startMaximized = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+        Configuration.remote = selenoidUrl + "/wd/hub/";
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        String sessionId = getSessionId();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        closeWebDriver();
+
+        Attach.addVideo(sessionId);
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+}
